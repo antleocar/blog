dbaf9fbdd1559167c488731e09db02654d52412d
antbadija <antonioleoncarrillo@gmail.com>
2016-05-22 19:31:39 +0200

    Update models

--- blog_app/blog_app/models/blog_record.py
+++ blog_app/blog_app/models/blog_record.py
@@ -8,6 +8,10 @@ from sqlalchemy import (
     DateTime,    #<- time abstraction field
     )
 
+from webhelpers2.text import urlify #<- will generate slugs
+from webhelpers2.date import distance_of_time_in_words #<- human friendly dates
+
+
 class BlogRecord(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
@@ -15,3 +19,12 @@ class BlogRecord(Base):
     body = Column(UnicodeText, default=u'')
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @property
+    def slug(self):
+        return urlify(self.title)
+
+    @property
+    def created_in_words(self):
+        return distance_of_time_in_words(self.created,
+                                         datetime.datetime.utcnow())
